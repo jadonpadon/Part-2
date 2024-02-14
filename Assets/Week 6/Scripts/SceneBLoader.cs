8599c0bcbea9ebf6e4ee554e2dc783c5b2895fae
salmarij <salmarij@OA-S141-15>
2024-02-14 10:52:04 -0500

    Week 6 task 2: scene switches using key input

---
+++ Assets/Week 6/Scripts/SceneBLoader.cs
@@ -0,0 +1,27 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneBLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+    }
+}
